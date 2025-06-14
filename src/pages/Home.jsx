55471773457abdc7bbabe3f52f3ff187a7c25586
mothibimalebana <mothibi.mm12@gmail.com>
2025-06-14 21:56:23 +0200

    chore: added links to navbar structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import Navbar from "../components/Navbar";
+
 const Home = () => {
     console.log('Hello World!')
     return(
         <>
-        <h1>react-starter-template</h1>
+        <Navbar/>
         </>
     )
 }
