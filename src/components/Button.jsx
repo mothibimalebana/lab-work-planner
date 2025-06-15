6b15a93c46df67dfd7b07c509d0ba262e1e07767
mothibimalebana <mothibi.mm12@gmail.com>
2025-06-15 20:19:51 +0200

    feat: filter button and updates button has been added to home page

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+const Buttons = ({buttonTitle}) => {
+    return (
+        <>
+        <button className="text-[14px]">
+            {buttonTitle}
+        </button>
+        </>
+    )
+}
+
+export default Buttons
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,19 @@ import CurrDate from "../components/Date";
 const Home = () => {
     return(
         <>
-            <div className="home flex pl-[20px] flex-col">
+            <div className="home flex pl-[20px] flex-col pr-[20px]">
                 <div className="breadCrumbs pt-[27px]">
                     <BreadCrumbs currPage="TimeTable" />
                 </div>
-                <div className="header pt-[16px]">
+                <div className="header flex items-center justify-between pt-[16px]">
                     <CurrDate/>
+                    <div className="buttons flex">
+                        <button className="button bg-[#FFFFFF] rounded-[4px] text-[#000000] h-[40px] w-[140px]">Filter</button>
+                        <button className="button bg-[#161127] rounded-[4px] h-[40px] w-[140px]">Updates</button>
+                    </div>
                 </div>
             </div>
         </>
     )
 }
-export default Home;
+export default Home
