6a60c469d1389b468fcbbede2aac56e232345c45
mothibimalebana <mothibi.mm12@gmail.com>
2025-11-22 20:17:58 +0200

    style: added margin top of 7

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
     const page = window.location.pathname.slice(1)
     return(
-        <div className="header px-[5.12rem]">
+        <div className="header px-[5.12rem] mt-7">
             <div className="breadCrumbs">
                 <h3 className={`text-black text-[0.875rem]`}><span className="text-[#DCDCDC]">{`Lab Work Planner >`}</span> { page } </h3>
             </div>
