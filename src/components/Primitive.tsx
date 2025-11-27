799b40303727c543190ced033a8af1ca19f691fe
mothibimalebana <mothibi.mm12@gmail.com>
2025-11-27 04:49:51 +0200

    style: changed font from dm-sans to arimo

--- src/components/Primitive.tsx
+++ src/components/Primitive.tsx
@@ -55,7 +55,7 @@ const Primitive = (  ) => {
             <table className="w-full rounded-md border border-solid border-[#E5E8EB] bg-white">
                 <tbody>
                     <tr className="rounded-lg border-b border-b-solid border-b-[#E5E8EB]">
-                    <th className="pl-[0.65rem] w-[2.13rem] bg-[rgba(236,236,240,0.30)]  text-left font-normal">Time</th>
+                    <th className="pl-[0.65rem] w-fit bg-[rgba(236,236,240,0.30)]  text-left font-normal">Time</th>
                         <th className="bg-[rgba(236,236,240,0.30)] text-[#0A0A0A] font-dm-sans text-[0.85163rem] not-italic font-normal leading-[1.27744rem] w-8.5 h-[2.59031rem] text-left">Monday</th>
                         <th className="bg-[rgba(236,236,240,0.30)] text-[#0A0A0A] font-dm-sans text-[0.85163rem] not-italic font-normal leading-[1.27744rem] w-8.5 h-[2.59031rem] text-left">Tuesday</th>
                         <th className="bg-[rgba(236,236,240,0.30)] text-[#0A0A0A] font-dm-sans text-[0.85163rem] not-italic font-normal leading-[1.27744rem] w-8.5 h-[2.59031rem] text-left">Wednsday</th>
@@ -64,25 +64,25 @@ const Primitive = (  ) => {
                         <th className="bg-[rgba(236,236,240,0.30)] text-[#0A0A0A] font-dm-sans text-[0.85163rem] not-italic font-normal leading-[1.27744rem] w-8.5 h-[2.59031rem] text-left">Saturday</th>
                         <th className="bg-[rgba(236,236,240,0.30)] text-[#0A0A0A] font-dm-sans text-[0.85163rem] not-italic font-normal leading-[1.27744rem] w-8.5 h-[2.59031rem] text-left">Sunday</th>
                     </tr>
-                    <tr className="rounded-lg border-b border-b-solid border-b-[#E5E8EB]">
-                        <td className="w-[2.13rem] bg-[rgba(236,236,240,0.30)]  text-left font-normal">08:00 <br />11:00</td>
+                    <tr className="rounded-lg font-[Arimo] border-b border-b-solid border-b-[#E5E8EB]">
+                        <td className="w-fit bg-[rgba(236,236,240,0.30)] text-left font-normal">08:00 <br />11:00</td>
                         {schedule[0].map((shift) => shift.shift ? <td className="mx-auto"><div className="working h-full flex  rounded-lg border border-solid border-[#337E89] bg-[rgba(142,184,190,0.20)]"><div className="cell p-[0.65rem] text-[0.74513rem]"> <h6>Lab Assistant</h6> <p>{shift.supervisor}</p> <p>Lab: {shift.lab}</p> </div></div></td> : <td><div className="freeSlot h-[80%] text-left text-[#99A1AF] rounded-lg border border-solid border-[#E5E8EB] cell p-[0.65rem] text-[0.74513rem]">Free slot</div></td>)}
                     </tr>
-                    <tr className="rounded-lg border-b border-b-solid border-b-[#E5E8EB]">
-                        <td className="w-[2.13rem] bg-[rgba(236,236,240,0.30)]  text-left font-normal">11:00 <br />14:00</td>
+                    <tr className="rounded-lg font-[Arimo] border-b border-b-solid border-b-[#E5E8EB]">
+                        <td className="w-fit bg-[rgba(236,236,240,0.30)]  text-left font-normal">11:00 <br />14:00</td>
                         {schedule[1].map((shift) => shift.shift ? <td><div className="working h-full flex  rounded-lg border border-solid border-[#337E89] bg-[rgba(142,184,190,0.20)] "><div className="cell p-[0.65rem] text-[0.74513rem]"> <h6>Lab Assistant</h6> <p>{shift.supervisor}</p> <p>Lab: {shift.lab}</p> </div></div></td> : <td><div className="freeSlot h-[80%] text-left text-[#99A1AF] rounded-lg border border-solid border-[#E5E8EB] cell p-[0.65rem] text-[0.74513rem]">Free slot</div></td>)}
                     </tr>
-                    <tr className="rounded-lg border-b border-b-solid border-b-[#E5E8EB]">
-                        <td className="w-[2.13rem] bg-[rgba(236,236,240,0.30)]  text-left font-normal">14:00 <br />17:00</td>
+                    <tr className="rounded-lg font-[Arimo] border-b border-b-solid border-b-[#E5E8EB]">
+                        <td className="w-fit bg-[rgba(236,236,240,0.30)]  text-left font-normal">14:00 <br />17:00</td>
                         {schedule[2].map((shift) => shift.shift ? <td><div className="working h-full flex  rounded-lg border border-solid border-[#337E89] bg-[rgba(142,184,190,0.20)] "><div className="cell p-[0.65rem] text-[0.74513rem]"> <h6>Lab Assistant</h6> <p>{shift.supervisor}</p> <p>Lab: {shift.lab}</p> </div></div></td> : <td><div className="freeSlot h-[80%] text-left text-[#99A1AF] rounded-lg border border-solid border-[#E5E8EB] cell p-[0.65rem] text-[0.74513rem]">Free slot</div></td>)}
 
                     </tr>
-                    <tr className="rounded-lg border-b border-b-solid border-b-[#E5E8EB]">
-                        <td className="w-[2.13rem] bg-[rgba(236,236,240,0.30)]  text-left font-normal">17:00 <br />20:00</td>
+                    <tr className="rounded-lg font-[Arimo] border-b border-b-solid border-b-[#E5E8EB]">
+                        <td className="w-fit bg-[rgba(236,236,240,0.30)]  text-left font-normal">17:00 <br />20:00</td>
                         {schedule[3].map((shift) => shift.shift ? <td><div className="cell p-[0.65rem] text-[0.74513rem]"> <h6>Lab Assistant</h6> <p>{shift.supervisor}</p> <p>Lab: {shift.lab}</p> </div></td> : <td><div className="freeSlot h-[80%] text-left text-[#99A1AF] rounded-lg border border-solid border-[#E5E8EB] cell p-[0.65rem] text-[0.74513rem]">Free slot</div></td>)}
                     </tr>
-                    <tr className="rounded-lg border-b border-b-solid border-b-[#E5E8EB]">
-                        <td className="w-[2.13rem] bg-[rgba(236,236,240,0.30)]  text-left font-normal">20:00 <br />00:00</td>
+                    <tr className="rounded-lg font-[Arimo] border-b border-b-solid border-b-[#E5E8EB]">
+                        <td className="w-fit bg-[rgba(236,236,240,0.30)]  text-left font-normal">20:00 <br />00:00</td>
                         {schedule[4].map((shift) => shift.shift ? <td><div className="working h-full flex  rounded-lg border border-solid border-[#337E89] bg-[rgba(142,184,190,0.20)] "><div className="cell p-[0.65rem] text-[0.74513rem]"> <h6>Lab Assistant</h6> <p>{shift.supervisor}</p> <p>Lab: {shift.lab}</p> </div></div></td> : <td><div className="freeSlot h-[80%] text-left text-[#99A1AF] rounded-lg border border-solid border-[#E5E8EB] cell p-[0.65rem] text-[0.74513rem]">Free slot</div></td>)}
                     </tr>
                 </tbody>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,7 +8,7 @@ const LoginPage: React.FC = () => {
         <div className="login-container bg-[background: linear-gradient(135deg,#F8FAFC_0%,#FFF_50%,#F0FDFA_100%),#FFF] w-full h-full flex justify-center items-center">
             <div className="login h-full flex flex-col justify-center w-md font-[Arimo] text-[#0A0A0A]">
                 <div className="exit flex gap-0.5"><img src={back} alt="" /> <p>Back</p></div>
-                <form className="flex p-6 rounded-sm border border-solid border-[rgba(0,0,0,0.10)] mt-5 flex-col" action="#" method="post">
+                <form className="flex p-6 rounded-lg border border-solid border-[rgba(0,0,0,0.10)] mt-5 flex-col" action="#" method="post">
                     <h5 className="text-[#0A0A0A]">Login</h5>
                     <p className="text-[#717182]">Sign in to your account</p>
                     <fieldset className="flex gap pt-6 flex-col">
