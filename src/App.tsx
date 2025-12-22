0aa35c2b237f029b31e4d868973a84e9bde6b032
mothibimalebana <mothibi.mm12@gmail.com>
2025-12-22 19:53:45 +0200

    fix: removed the state inside App.tsx
    No longer using outlet to pass down data, App.tsx now uses context to pass data

--- src/App.tsx
+++ src/App.tsx
@@ -3,36 +3,21 @@ import Header from "./components/Header"
 import Navbar from "./components/Navbar"
 import phoneError from "./assets/svg/phone-error-svgrepo-com.svg"
 import { useState } from "react";
-import { appSchedule, mockStudents, updateSchedule } from "./assets/mockData";
-import type { outletContextProp } from "../types/context";
+
 
 function App() {  
-  //global state
-  const [schedule, setSchedule] = useState(appSchedule);
-  const [students, setStudents] = useState(mockStudents);
+  //Get
+  const getPathname = window.location.pathname.slice(5);
+  const pathNameStr = getPathname[0].toUpperCase() + getPathname.slice(1);
 
-  const outletContext = {
-    schedule: schedule,
-    setSchedule: setSchedule,
-
-    students: students,
-    setStudents: setStudents,
-
-    newSchedule: updateSchedule,
-  }
-
-  const getPage = window.location.pathname.slice(5);
-  console.log(getPage);
-  const currPage = getPage[0].toUpperCase() + getPage.slice(1);
-
-  const [page, setPage] = useState(currPage);
+  const [pathname, setPathname] = useState(pathNameStr);
 
   return (
     <div className="app">
       <div className="desktop hidden md:flex flex-col overflow-x-hidden">
-        <Navbar setLink={setPage}/>
-        <Header page={page}/>
-        <Outlet context={outletContext satisfies outletContextProp}/>
+        <Navbar setLink={setPathname}/>
+        <Header page={pathname}/>
+        <Outlet/>
       </div>
       <div className="mobile flex flex-col md:hidden">
         <div className="icon"><img src={phoneError} alt="disconnected phone"/></div>
